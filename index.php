519bdf76897abbc65f0cbe431075fc7e158ddf79
claudiopb <hai_madre@hotmail.com>
2024-09-27 02:36:04 -0600

    index.php

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <p>hola</p>
+  <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Free</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0<small class="text-body-secondary fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Pro</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$15<small class="text-body-secondary fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>20 users included</li>
+              <li>10 GB of storage</li>
+              <li>Priority email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+          <div class="card-header py-3 text-bg-primary border-primary">
+            <h4 class="my-0 fw-normal">Enterprise</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$29<small class="text-body-secondary fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>30 users included</li>
+              <li>15 GB of storage</li>
+              <li>Phone and email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ index1.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container-fluid pt-3 my-6 bg-warning text-dark">
+       <h1>My                       First                 Bootstrap                     Page</h1>
+       <p>This part is inside a .container-fluid class.</p>  
+       <p>The .container-fluid class provides a full width container, spanning the entire width of the viewport.</p>           
+  </div>
+
+  <div class="container my-3 bg-primary text-white">
+       <h1>Mi segundo container Bootstrap</h1>
+       <p>This part is inside a .container-fluid class.</p>  
+       <p>The .container-fluid class provides a full width container, spanning the entire width of the viewport.</p>           
+  </div>
+
+  <div class="container p-3 my-3 border">
+       
+  </div>
+
+<div class="container-sm my-3 bg-primary text-white">.container-sm</div>
+<div class="container-md my-3 bg-primary text-white">.container-md</div>
+<div class="container-lg my-3 bg-primary text-white">.container-lg</div>
+<div class="container-xl my-3 bg-primary text-white">.container-xl</div>
+
+<div class="container p-3 my-3 border">
+<div class="row">
+ 
+ 
+  <div class="col-sm-4 bg-warning text-dark">
+  <img src="./foto/moto3.jfif" class="rounded" alt="Cinque Terre" width="150" height="150">
+  </div>
+  <div class="col-sm-4 bg-warning text-dark">
+  <img src="./foto/moto1.jfif" class="rounded" alt="Cinque Terre" width="150" height="150"> 
+  </div>
+  <div class="col-sm-4 bg-warning text-dark">
+  <img src="./foto/moto2.jfif" class="rounded" alt="Cinque Terre" width="150" height="150">
+  </div>
+  <div class="col-sm-4 bg-warning text-dark">
+  <img src="./foto/moto4.jfif" class="rounded" alt="Cinque Terre" width="150" height="150">
+  </div>
+  <div class="col-sm-4 bg-warning text-dark">
+  <img src="./foto/moto4.jfif" class="rounded" alt="Cinque Terre" width="150" height="150">
+  </div>
+  <div class="col-sm-4 bg-warning text-dark">
+  <img src="./foto/moto4.jfif" class="rounded" alt="Cinque Terre" width="150" height="150">
+  </div>
+
+</div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+</html>
