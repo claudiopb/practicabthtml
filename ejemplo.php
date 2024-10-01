81404393491cb81d6e3e59f5e2392c2ae8dc8d1d
claudiopb <hai_madre@hotmail.com>
2024-10-01 02:26:00 -0600

    co

---
+++ ejemplo.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+    <title>Bootstrap Example</title>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+  </head>
+  <body class="p-3 m-0 border-0 bd-example m-0 border-0 bd-example-cols">
+
+    <!-- Example Code -->
+    
+        
+    <div class="container overflow-hidden text-center">
+      <div class="row gy-2">
+        <div class="col-6">
+          <div class="p-1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Relleno de columna personalizado</font></font></div>
+        </div>
+        <div class="col-6">
+          <div class="p-1"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Relleno de columna personalizado</font></font></div>
+        </div>
+        <div class="col-6">
+          <div class="p-3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Relleno de columna personalizado</font></font></div>
+        </div>
+        <div class="col-6">
+          <div class="p-3"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Relleno de columna personalizado</font></font></div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="container mt-3">
+  <h2>Card Image</h2>
+  <p>Image at the top (card-img-top):</p>
+  <div class="card" style="width:300px">
+    <img class="mx-auto d-block" class="card-img-top" src="./foto/moto1.jfif" alt="Card image" style="width:75%">
+    <div class="card-body">
+      <h4 class="card-title">John Doe</h4>
+      <p class="card-text">Some example text some example text. John Doe is an architect and engineer</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+  <br>
+  
+  
+</div>
+
+
+      
+    <!-- End Example Code -->
+  </body>
+</html>
--- estructura_web_bootstrap.php
+++ estructura_web_bootstrap.php
@@ -7,7 +7,22 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hola chicos de 5 "C" esta es la plantilla que pueden usar para sus practicas le pueden cambiar el nombre al descargarla</h1>
+  <div class="container overflow-hidden text-center">
+  <div class="row gy-5">
+    <div class="col-6">
+      <div class="p-3">Custom column padding</div>
+    </div>
+    <div class="col-6">
+      <div class="p-3">Custom column padding</div>
+    </div>
+    <div class="col-6">
+      <div class="p-3">Custom column padding</div>
+    </div>
+    <div class="col-6">
+      <div class="p-3">Custom column padding</div>
+    </div>
+  </div>
+</div>  
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </body>
 </html>
