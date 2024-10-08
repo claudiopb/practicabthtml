b937dfd9177be1779fed2ff01cfc170bf8587331
claudiopb <hai_madre@hotmail.com>
2024-10-08 13:40:34 -0600

    Create prac1.php

---
+++ practicaphpconbd/prac1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+//echo "<h2>$txt1</h2>"
+echo '<h2>' . $txt1 . '</h2>';
+?>
+<br></br>
+
+</body>
+</html>
