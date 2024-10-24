d2e6aee5ab75ee8bbab20b38a1c0678b6f8eff70
claudiopb <hai_madre@hotmail.com>
2024-10-24 11:59:51 -0600

    Create fetch-row.php

---
+++ practicaphpconbd/fetch-row.php
@@ -0,0 +1,33 @@
+<?php
+$mysqli = new mysqli("localhost","root","","test");
+
+if ($mysqli -> connect_errno) {
+  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+  exit();
+}
+
+$sql = "SELECT * FROM alumno";
+/*
+if ($result = $mysqli -> query($sql)) {
+  $row = $result -> fetch_row();
+    print "$row[0]" ."  ". "$row[1] "."  ". "$row[3] <br>";
+
+    $row = $result -> fetch_row();
+    print "$row[0]" ."  ". "$row[1] "." ". "$row[3] <br>";
+  
+    
+    $row = $result -> fetch_row();
+    print "$row[0]" ."  ". "$row[1] "." ". "$row[3] <br>";
+*/
+  
+if ($result = $mysqli -> query($sql)) {
+   while($row = $result -> fetch_row()){
+      print "$row[0]" ."  ". "$row[1] "."  ". "$row[3] <br>";
+   }
+
+  
+  $result -> free_result();
+}
+
+$mysqli -> close();
+?>
